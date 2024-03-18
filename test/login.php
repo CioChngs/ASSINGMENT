0a835d9ee09b8cc43ccee0d3daa3c0a4037eb0d7
CioChngs <csciodev0@gmail.com>
2024-03-18 14:36:28 +0800

    hash,

--- test/login.php
+++ test/login.php
@@ -11,7 +11,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $user = $stmt->fetch();
 
         if ($user) {
-            if ($password === $user['password']) {
+            if (password_verify($password, $user['password'])) {
                 session_start();
                 $_SESSION['username'] = $username;
                 header("Location: order.html");
--- test/signup.php
+++ test/signup.php
@@ -8,15 +8,17 @@ try {
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $username = $_POST['username'];
         $email = $_POST['email'];
-        $phone = $_POST['phone']; 
-        $password = $_POST['password']; 
+        $phone = $_POST['phone'];
+        $password = $_POST['password'];
+
+        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
         $stmt = $pdo->prepare("INSERT INTO Customer (username, email, phone_number, password) VALUES (:username, :email, :phone, :password)");
 
         $stmt->bindParam(':username', $username);
         $stmt->bindParam(':email', $email);
         $stmt->bindParam(':phone', $phone);
-        $stmt->bindParam(':password', $password);
+        $stmt->bindParam(':password', $hashed_password);
         $stmt->execute();
 
         echo "User registered successfully!";
